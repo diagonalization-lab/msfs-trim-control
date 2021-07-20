68fbbc4a3ef80a044b6f327f19e6f51913abc9b5
Diagonalization <diagonalization@fastmail.com>
2021-07-19 22:31:37 -0400

    A hack to clip elevator trim.

--- html_ui/InGamePanels/diagonalization-panel-indicatorsPanel/main.js
+++ html_ui/InGamePanels/diagonalization-panel-indicatorsPanel/main.js
@@ -1,17 +1,53 @@
+// The interval at which the main loop runs.
+var pollIntervalMs = 100;
+
+// Maximum change in elevator trim (in degrees) per loop iteration.
+var maxElevatorTrimDelta = 0.01;
+
+// The last observed elevator trim position.
+var lastElevatorTrimPosition = 0;
+
+function getElevatorTrimPosition() {
+    return SimVar.GetSimVarValue("ELEVATOR TRIM POSITION", "degrees");
+}
+
+
+function setElevatorTrimPosition(newPosition, forceSet) {
+    lastElevatorTrimPosition = newPosition;
+    // Produces a promise that we ignore.
+    if (forceSet) {
+        SimVar.SetSimVarValue("ELEVATOR TRIM POSITION", "degrees", newPosition);
+    }
+    const elevatorTrimString = `${Math.round(newPosition * 100) / 100}`;
+    document.getElementById('elevatorTrim').innerText = elevatorTrimString;
+
+}
+
+function mainLoop() {
+    if (GetInputStatus("PLANE", "KEY_TOGGLE_WATER_RUDDER") !== 0) {
+        setElevatorTrimPosition(0, true);
+        return;
+    }
+    const currentElevatorTrimPosition = getElevatorTrimPosition();
+    const delta = currentElevatorTrimPosition - lastElevatorTrimPosition;
+    if (Math.abs(delta) > maxElevatorTrimDelta) {
+        console.log("Clipping elevator trim");
+        setElevatorTrimPosition(
+            lastElevatorTrimPosition + Math.sign(delta) * maxElevatorTrimDelta,
+            true);
+    }
+    else {
+        setElevatorTrimPosition(currentElevatorTrimPosition, false);
+    }
+}
+
 class IngamePanelDiagonalizationPanelIndicatorsPanel extends HTMLElement {
     constructor() {
         super();
     }
     connectedCallback() {
-        setInterval(() => {
-            if (GetInputStatus("PLANE", "KEY_TOGGLE_WATER_RUDDER") !== 0) {
-                // Produces a promise that we ignore.
-                SimVar.SetSimVarValue("ELEVATOR TRIM POSITION", "degrees", 0);
-            }
-            const elevatorTrim = SimVar.GetSimVarValue("ELEVATOR TRIM POSITION", "degrees");
-            const elevatorTrimString = `${Math.round(elevatorTrim * 100) / 100}`;
-            document.getElementById('elevatorTrim').innerText = elevatorTrimString;
-        }, 500);
+        lastElevatorTrimPosition = getElevatorTrimPosition();
+        setInterval(mainLoop, pollIntervalMs);
     }
     updateImage() {
     }
@@ -21,4 +57,4 @@ window.customElements.define(
     "ingamepanel-diagonalization-panel-indicators",
     IngamePanelDiagonalizationPanelIndicatorsPanel);
 
-    checkAutoload();
+checkAutoload();
